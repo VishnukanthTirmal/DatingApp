083549027d06cf64e022225c19cc42f89f6ea63c
Vishnukanth Tirmal <t.vishnukanth@gmail.com>
2018-01-25 06:49:20 +0000

    Update User details to database from angular spa

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -5,7 +6,7 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
 
 namespace DatingApp.API.Controllers
 {
@@ -36,6 +37,28 @@ namespace DatingApp.API.Controllers
             var userToReturn =_mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
         }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser (int id, [FromBody] UserForUpdateDto userForUpdateDto)
+        {
+            if(ModelState.IsValid== false)
+              return BadRequest(ModelState);
 
+             var curentUserId= int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+             var userFromRepo= await _repo.GetUser(id);
+
+            if(userFromRepo == null)
+               return NotFound($"Cound not find user with an id if {id}");
+
+            if(curentUserId != userFromRepo.Id)
+                return Unauthorized();
+
+                _mapper.Map(userForUpdateDto, userFromRepo);
+
+                if(await _repo.SaveAll())
+                    return NoContent();
+
+                throw new  Exception($"Updating user {id} failed on save");
+        }
     }
 }
--- DatingApp.SPA/src/app/_services/User.service.ts
+++ DatingApp.SPA/src/app/_services/User.service.ts
@@ -28,6 +28,9 @@ getUser(id): Observable <User> {
     .map(response => <User>response.json())
     .catch(this.handleError);
   }
+  updatedUser(id: number, user: User) {
+      return this.authHttp.put(this.baseUrl + 'users/' + id, user).catch(this.handleError);
+  }
 
   private handleError(error: any) {
     const applicationError = error.headers.get('Application-Error');
